d9c13a771072bb2028c47a2cada76e0e031947f6
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-27 16:12:09 +0100

    exclude better nether & end anvils

--- 1.20.4/Common/src/main/java/fuzs/easyanvils/data/ModBlockTagsProvider.java
+++ 1.20.4/Common/src/main/java/fuzs/easyanvils/data/ModBlockTagsProvider.java
@@ -4,8 +4,13 @@ import fuzs.easyanvils.init.ModRegistry;
 import fuzs.puzzleslib.api.data.v2.AbstractTagProvider;
 import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+
+import java.util.List;
 
 public class ModBlockTagsProvider extends AbstractTagProvider.Blocks {
+    private static final List<String> UNALTERED_ANVILS = List.of("betterend:aeternium_anvil", "betterend:terminite_anvil", "betterend:thallasium_anvil", "betternether:cincinnasite_anvil");
 
     public ModBlockTagsProvider(DataProviderContext context) {
         super(context);
@@ -13,6 +18,7 @@ public class ModBlockTagsProvider extends AbstractTagProvider.Blocks {
 
     @Override
     public void addTags(HolderLookup.Provider provider) {
-        this.tag(ModRegistry.UNALTERED_ANVILS_BLOCK_TAG);
+        IntrinsicTagAppender<Block> tagAppender = this.tag(ModRegistry.UNALTERED_ANVILS_BLOCK_TAG);
+        UNALTERED_ANVILS.stream().map(ResourceLocation::new).forEach(tagAppender::addOptional);
     }
 }
